5292383d7651a776513056e9bb887e837ec4afd5
hp <hp@DESKTOP-392489H>
2024-09-29 17:40:49 +0530

    Commit 1

---
+++ JenkinsProject/src/dummyPackage/RegistrationTest.java
@@ -0,0 +1,22 @@
+package dummyPackage;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTest 
+{
+@Test
+public void testcase1()
+{
+	System.out.println("This is my first testcase1");
+}
+@Test
+public void testcase2()
+{
+	System.out.println("This is my first testcase2");
+}
+@Test
+public void testcase3()
+{
+	System.out.println("This is my first testcase3");
+}
+}
